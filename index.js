f46e7a4e4e3177f3c6ce4a58c0f74d6aaffb6deb
cs2338139 <cs2338139@gmail.com>
2022-02-18 16:14:36 +0800

    enter

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ let camera, scene, renderer;
 let geometry, mesh;
 let material;
 let controls;
+let step;
+var pointerState;
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+var buttonPlaneList = new Array();
+var buttonList = new Array();
 
 init();
+render();
+window.addEventListener("pointerdown", ClickButton);
+window.addEventListener("pointermove", GetButton);
 
 function init() {
   scene = new THREE.Scene();
@@ -13,12 +22,15 @@ function init() {
   renderer.setClearColor("#ffffff");
   document.body.appendChild(renderer.domElement);
 
-  let texture = new THREE.TextureLoader().load("img_1.jpeg");
+  let texture = new THREE.TextureLoader().load("img/01.jpg");
+  // texture.flipX=false;
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.repeat.x = -1;
   material = new THREE.MeshBasicMaterial({
     map: texture,
     side: THREE.BackSide,
   });
-  geometry = new THREE.SphereGeometry(1000, 64, 64);
+  geometry = new THREE.SphereGeometry(500, 64, 64);
   mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
 
@@ -28,23 +40,61 @@ function init() {
     45,
     30000
   );
-  camera.position.set(0, 0, 500);
+  camera.position.set(0, 0, 250);
 
   controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.enabled = true;
-  controls.minDistance = 100;
-  controls.maxDistance = 700;
+  controls.minDistance = 250;
+  controls.maxDistance = 250;
   controls.enablePan = false;
-  // controls.autoRotate = true;
-  // controls.autoRotateSpeed = 1.0;
+
+  step = 1;
+  CreateButton(step);
+}
+function CheckStep() {}
+
+function GetButton(event) {
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(pointer, camera);
+
+  const intersects = raycaster.intersectObjects(buttonPlaneList);
+  for (var i = 0; i < intersects.length; i++) {
+    for (var j = 0; j < buttonPlaneList.length; j++) {
+      if (intersects[i].object.name===buttonPlaneList[j].name) {
+        console.log(buttonPlaneList[j].name);
+      }
+    }
+  }
 }
 
-animate();
+function ClickButton(event) {}
 
-function animate() {
-  requestAnimationFrame(animate);
+function CreateButton(step) {
+  switch (step) {
+    case 1:
+      const geometry = new THREE.PlaneGeometry(50, 50);
+      const material = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        side: THREE.DoubleSide,
+      });
+      const plane = new THREE.Mesh(geometry, material);
+      plane.position.set(0, 0, 0);
+      plane.name="buttonPlaneList_0";
+
+      buttonPlaneList.push(plane);
+      scene.add(plane);
+      plane.visible=false；
+
+      break;
+
+    default:
+      break;
+  }
+}
+
+function render() {
+  requestAnimationFrame(render);
   renderer.render(scene, camera);
-
-  // console.log(a1 + "  " + a2 + "  " + l);
-  // console.log(light.))
 }
